eb49f14571ee1fd13d124212ca51dcaa963f0f81
Priyanshu Banerjee <priyanshu.ban2003@gmail.com>
2023-09-21 23:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
 
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    cartItem.classList.remove('active');
+}
+
+
+window.onscroll = () => {
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
 }
